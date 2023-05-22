9984e51ac215b7564623852188cb623fda320be1
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-22 13:44:21 -0700

    add completed property for items and update items/edit view

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -7,6 +7,7 @@ namespace ToDoList.Models
     public int ItemId { get; set; }
     public string Description { get; set; }
     public int CategoryId { get; set; }
+    public bool Completed { get; set; }
     public Category Category { get; set; }
     public List<ItemTag> JoinEntities { get;} //collection navigation property
   }
