25f3ba988f953f566d24a93dbe7fb7675892eb42
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-22 10:50:12 -0700

    add tags controller with read functionality

--- ToDoList/Controllers/CategoriesController.cs
+++ ToDoList/Controllers/CategoriesController.cs
@@ -37,7 +37,9 @@ namespace ToDoList.Controllers
     public ActionResult Details(int id)
     {
       Category thisCategory = _db.Categories
-                                  .Include(category => category.Items)
+                                  .Include(category => category.Items) //cat? bottom lesson 5
+                                  .ThenInclude(item => item.JoinEntities)
+                                  .ThenInclude(join => join.Tag)
                                   .FirstOrDefault(category => category.CategoryId == id);
       return View(thisCategory);
     }
