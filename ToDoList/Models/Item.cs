c2f7530b6b0dd64afe6e9b8db8c69ebe6b291032
Gareth Grindeland <gt.grindeland@gmail.com>
2023-05-23 11:25:13 -0700

    add data validation for item model

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -1,12 +1,15 @@
 using System.Collections.Generic;
 using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace ToDoList.Models
 {
   public class Item
   {
     public int ItemId { get; set; }
+    [Required(ErrorMessage = "The item's description can't be empty!")]
     public string Description { get; set; }
+    [Range(1, int.MaxValue, ErrorMessage = "You must add your item to a category. Have you created a category yet?")]
     public int CategoryId { get; set; }
     public bool Completed { get; set; }
     public DateTime DueDate { get; set; }
